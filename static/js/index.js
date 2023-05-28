62f04cf471cd3ace17c54632a7c8e3ba9cabd030
NewCodemao <122192283+NewCodemao@users.noreply.github.com>
2023-05-28 18:24:01 +0800

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,6 @@
 function loadMySelf() {
     myself = document.getElementById("myself");
-    if (!document.cookie) {
+    if (document.cookie == "" || document.cookie == 0 || !Boolean(document.cookie)) {
         myself.innerHTML = "<a href=\"./login\">请先登录</a>";
     } else {
         myself.innerHTML = "<a href=\"./my\">我的</a>";
